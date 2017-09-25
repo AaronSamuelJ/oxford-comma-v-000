d9c25ea2ec7f3cf2016e9de075f15a92592a38fa
Aaron Samuel <aasamuelj@gmail.com>
2017-09-25 00:44:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(arr)
     arr.join
   elsif arr.size == 2
     arr.join(" and ")
-  elsif arr.size == 3
+  elsif arr.size >= 3
     last = arr.pop
     join_arr = arr.join(", ")
     "#{join_arr}, and #{last}"

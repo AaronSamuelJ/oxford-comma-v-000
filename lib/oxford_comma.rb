8f061ee3982055d7858a99136ed5bd283fac306d
Aaron Samuel <aasamuelj@gmail.com>
2017-09-25 00:43:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,12 @@
 require 'pry'
 def oxford_comma(arr)
-  binding.pry
-  arr.join(" and ")
-  last_element = arr.pop
+  if arr.size == 1
+    arr.join
+  elsif arr.size == 2
+    arr.join(" and ")
+  elsif arr.size == 3
+    last = arr.pop
+    join_arr = arr.join(", ")
+    "#{join_arr}, and #{last}"
+  end
 end
